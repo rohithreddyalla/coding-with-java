accf5b6ff786c5ff319fb692f94d773edc536c6a
rohithreddyalla <90763752+rohithreddyalla@users.noreply.github.com>
2021-09-23 23:08:30 +0530

    Update and rename 1.java to hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  
+  public class Main
+ {
+  
+  public static void main(String[] args)
+  {
+    System.out.print("hello world");
+    System.out.print("hello world");
+    System.out.println("hello world");//ln used to print in separate line
+    System.out.print("hello world\n");//or we can use \n a special char
+    
+  }
+ }
