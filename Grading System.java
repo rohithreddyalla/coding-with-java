4a9e96ceff94f78548de51f92ee4a4e2774adf4c
rohithreddyalla <90763752+rohithreddyalla@users.noreply.github.com>
2021-09-23 23:17:09 +0530

    Create Grading System.java
    1. You are given as input marks of a student.
    2. Display an appropriate message based on the following rules:
      	  2.1 for marks above 90, print excellent.
      	  2.2 for marks above 80 and less than equal to 90, print good.
      	  2.3 for marks above 70 and less than equal to 80, print fair.
      	  2.4 for marks above 60 and less than equal to 70, print meets expectations.
      	  2.5 for marks less than equal to 60, print below par.
    
    Note -> Only change the code in area after - // code here
    Input Format
    Input is handled for you and provided as variable marks
    Output Format
    Appropriate message as per student's marks
    Sample Input
    92
    Sample Output
    excellent

---
+++ Grading System.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      // input - don't change this code
+      Scanner scn = new Scanner(System.in);
+      int marks = scn.nextInt();
+      // input - don't change this code
+      
+      // code here      
+      }
+  
+   }
+  }
