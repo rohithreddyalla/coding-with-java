811d3fc820e283107fa5e9bad0a268461fd62632
rohithreddyalla <90763752+rohithreddyalla@users.noreply.github.com>
2021-09-27 23:46:12 +0530

    Add files via upload
    Questions:
    
    You have to display the digits of a number.
    Take as input "n" , the number for which digits have to be displayed.
    Print the digits of the number line-wise.
    Input Format:
    
    "n" where n is any integer.
    
    Output Format:
    
    d1
    
    d2
    
    d3
    
    ... digits of the number.
    
    Constraints
    
    1 <= n < 10^9
    
    Solution Approach
    
    We have to print a given number from left to right in a vertical fashion.
    
    For instance , let take 754
    
    We start dividing it with 100  [(10)3^1] where 3 is the number of digits in 754.
    
    After each division the divisor is divided by 10 .

---
+++ 8)Digits Of A Number.java
@@ -0,0 +1,32 @@
+import java.util.*;
+    
+    public class Main {
+    
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+    
+            int power = 1;
+            int temp = n;234
+            while (temp >= 10) //234> 23>
+            {
+                temp = temp/10; //23 2
+                power = power*10; //10 100
+            }
+    
+            temp = n; //again temp = 234
+            while(power != 0) //100
+            {
+                int digit = temp / power; // 234/100=2 
+                System.out.println(digit); // 2  
+    
+                temp = temp % power; // 34
+                power = power / 10; // power=10
+            }
+        }
+    }
+
+
+                        
+                        
+                        
